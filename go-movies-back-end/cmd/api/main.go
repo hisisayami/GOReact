7228f7a7aa00a6c01380006bfc5a69bba67ed106
hisi <hisilasayami@gmail.com>
2024-02-04 17:40:05 -0500

    Initialize db connection using docker

--- go-movies-back-end/cmd/api/main.go
+++ go-movies-back-end/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,22 +11,31 @@ import (
 const port = 8088
 
 type application struct {
+	DSN    string //Data Source Name
 	Domain string
+	DB     *sql.DB
 }
 
 func main() {
 	//set application config
 	var app application
-	 
+
 	//read from command line
+	flag.StringVar(&app.DSN, "dns", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
 
 	//connect to the database
-	app.Domain = "example.com"
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
+	defer app.DB.Close()
 
 	log.Println("Starting applicaiton on port", port)
 
 	//start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
