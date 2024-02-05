bf944a9d852a7eab26b427dc73c2dfebfd2b58b6
hisi <hisilasayami@gmail.com>
2024-02-04 20:03:33 -0500

    Work with JSON

--- go-movies-back-end/cmd/api/handlers.go
+++ go-movies-back-end/cmd/api/handlers.go
@@ -1,76 +1,30 @@
 package main
 
 import (
-	"backend/internal/models"
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	var payload = struct {
-		Status string `json:"status"`
+		Status  string `json:"status"`
 		Message string `json:"message"`
 		Version string `json:"version"`
 	}{
-		Status: "active",
+		Status:  "active",
 		Message: "Go Movies up and running",
 		Version: "1.0.0",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
-func (app *application) AllMovies(w http.ResponseWriter, r *http.Request){
-	var movies []models.Movie
-
-	rd, _ := time.Parse("2006-08-24", "1998-03-04")
-
-	highlander := models.Movie {
-		ID: 1,
-		Title: "Highlander",
-		ReleaseDate: rd,
-		MPAARating: "R",
-		RunTime: 116,
-		Description: "action movie",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-
-	movies = append(movies, highlander)
-
-	rd, _ = time.Parse("2006-08-24", "1981-06-04")
-
-
-	rota := models.Movie {
-		ID: 2,
-		Title: "Raiders of the Lost Ark",
-		ReleaseDate: rd,
-		MPAARating: "PG-13",
-		RunTime: 120,
-		Description: "adventure movie",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-
-	movies = append(movies, rota)
-
-	out, err := json.Marshal(movies)
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.DB.AllMovies()
 	if err != nil {
-		fmt.Println(err)
+		app.errorJSON(w, err)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
-
+	_ = app.writeJSON(w, http.StatusOK, movies)
 
 }
